14bb7b1d04ceb9fc2c2e11b6cfa81a296d3a5c2e
Iain05 <58150364+Iain05@users.noreply.github.com>
2021-04-06 18:34:01 -0700

    Create CanCan.cpp

---
+++ Music/CanCan.cpp
@@ -0,0 +1,164 @@
+//----------------------------------------------------------------------------
+//                                                                            
+//    Module:       main.cpp                                                  
+//    Author:       {author}                                                  
+//    Created:      {date}                                                    
+//    Description:  IQ project                                                
+//                                                                            
+//----------------------------------------------------------------------------
+
+// Include the IQ Library
+#include "iq_cpp.h"
+
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+int main() {
+  // Begin project code
+  Brain.playNote(3, 4, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 2, 50);
+  Brain.playNote(4, 1, 100);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 3, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(5, 0, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 4, 300);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(3, 6, 150);
+  Brain.playNote(3, 6, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  //repeat
+  Brain.playNote(3, 4, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 2, 50);
+  Brain.playNote(4, 1, 100);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 3, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(5, 0, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 4, 300);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(3, 6, 150);
+  Brain.playNote(3, 6, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  //second repeat ending
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 0, 150);
+  Brain.playNote(4, 0, 300);
+
+  Brain.playNote(5, 2, 300);
+  Brain.playNote(5, 0, 300);
+  Brain.playNote(4, 5, 300);
+  Brain.playNote(4, 4, 300);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 3, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 0, 300);
+
+  
+  Brain.playNote(5, 2, 300);
+  Brain.playNote(5, 0, 300);
+  Brain.playNote(4, 5, 300);
+  Brain.playNote(4, 4, 300);
+  Brain.playNote(4, 3, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 6, 150);
+  Brain.playNote(5, 1, 150);
+  Brain.playNote(5, 0, 150);
+  Brain.playNote(5, 0, 300);
+
+  Brain.playNote(5, 2, 300);
+  Brain.playNote(5, 0, 300);
+  Brain.playNote(4, 5, 300);
+  Brain.playNote(4, 4, 300);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 3, 150);
+  Brain.playNote(4, 2, 150);
+  Brain.playNote(4, 1, 150);
+  Brain.playNote(4, 0, 300);
+
+  
+  Brain.playNote(5, 2, 300);
+  Brain.playNote(5, 0, 300);
+  Brain.playNote(4, 5, 300);
+  Brain.playNote(4, 4, 300);
+  Brain.playNote(4, 3, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 5, 150);
+  Brain.playNote(4, 6, 150);
+  Brain.playNote(5, 0, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 6, 150);
+  Brain.playNote(4, 4, 150);
+
+  Brain.playNote(5, 0, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 6, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(5, 0, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 6, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(5, 0, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(4, 6, 150);
+  Brain.playNote(4, 4, 150);
+  Brain.playNote(5, 0, 225);
+
+  Brain.playNote(4, 0, 75);
+  Brain.playNote(4, 0, 225);
+  Brain.playNote(4, 0, 75);
+  Brain.playNote(4, 0, 225);
+  Brain.playNote(4, 0, 75);
+  Brain.playNote(4, 0, 225);
+  Brain.playNote(4, 0, 75);
+  Brain.playNote(4, 0, 225);
+  Brain.playNote(4, 0, 75);
+  Brain.playNote(4, 0, 225);
+  Brain.playNote(4, 0, 75);
+  Brain.playNote(4, 0, 225);
+  Brain.playNote(4, 0, 75);
+  Brain.playNote(4, 0, 225);
+  Brain.playNote(4, 1, 75);
+  Brain.playNote(3, 3, 600);
+  
+}
