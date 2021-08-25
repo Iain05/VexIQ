8ce68e97c4d03fb167027b9d5f4edab629230c55
Iain05 <58150364+Iain05@users.noreply.github.com>
2021-08-25 11:45:56 -0700

    Create OdetoJoy.cpp

---
+++ Music/OdetoJoy.cpp
@@ -0,0 +1,83 @@
+//----------------------------------------------------------------------------
+//                                                                            
+//    Module:       main.cpp                                                  
+//    Author:       {author}                                                  
+//    Created:      {date}                                                    
+//    Description:  IQ project                                                
+//                                                                            
+//----------------------------------------------------------------------------
+
+// Include the IQ Library
+#include "iq_cpp.h"
+
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+int main() {
+  // Begin project code
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 3, 300);
+  Brain.playNote(3, 4, 300);
+  Brain.playNote(3, 4, 300);
+  Brain.playNote(3, 3, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 2, 450);
+  Brain.playNote(3, 1, 150);
+  Brain.playNote(3, 1, 600);
+  
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 3, 300);
+  Brain.playNote(3, 4, 300);
+  Brain.playNote(3, 4, 300);
+  Brain.playNote(3, 3, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 1, 450);
+  Brain.playNote(3, 0, 150);
+  Brain.playNote(3, 0, 600);
+
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 2, 150);
+  Brain.playNote(3, 3, 150);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 2, 150);
+  Brain.playNote(3, 3, 150);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(2, 4, 300);
+  Brain.playNote(3, 2, 600);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 3, 300);
+  Brain.playNote(3, 4, 300);
+  Brain.playNote(3, 4, 300);
+  Brain.playNote(3, 3, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 0, 300);
+  Brain.playNote(3, 1, 300);
+  Brain.playNote(3, 2, 300);
+  Brain.playNote(3, 1, 450);
+  Brain.playNote(3, 0, 150);
+  Brain.playNote(3, 0, 600);
+
+}
