22ff6f23012a14c73f8617a48bd0ed391bd4d3f2
Iain05 <58150364+Iain05@users.noreply.github.com>
2021-09-25 16:20:23 -0700

    Create USSRanthem.cpp

---
+++ Music/USSRanthem.cpp
@@ -0,0 +1,117 @@
+//----------------------------------------------------------------------------
+//                                                                            
+//    Module:       main.cpp                                                  
+//    Author:       {author}                                                  
+//    Created:      {date}                                                    
+//    Description:  IQ project                                                
+//                                                                            
+//----------------------------------------------------------------------------
+
+// Include the IQ Library
+#include "iq_cpp.h"
+
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+int main() {
+  // Begin project code
+  Brain.playNote(3, 4, 400);
+  Brain.playNote(4, 0, 800);
+  Brain.playNote(3, 4, 600);
+  Brain.playNote(3, 5, 200);
+  Brain.playNote(3, 6, 800);
+  Brain.playNote(3, 2, 400);
+  Brain.playNote(3, 2, 400);
+  Brain.playNote(3, 5, 800);
+  Brain.playNote(3, 4, 600);
+  Brain.playNote(3, 3, 200);
+  Brain.playNote(3, 4, 800);
+  Brain.playNote(3, 0, 400);
+  Brain.playNote(3, 0, 400);
+  Brain.playNote(3, 1, 800);
+  Brain.playNote(3, 1, 600);
+  Brain.playNote(3, 2, 200);
+  Brain.playNote(3, 3, 800);
+  Brain.playNote(3, 3, 600);
+  Brain.playNote(3, 4, 200);
+  Brain.playNote(3, 5, 800);
+  Brain.playNote(3, 6, 400);
+  Brain.playNote(4, 0, 400);
+  Brain.playNote(4, 1, 1200);
+
+  Brain.playNote(3, 4, 400);
+  Brain.playNote(4, 2, 800);
+  Brain.playNote(4, 1, 600);
+  Brain.playNote(4, 0, 200);
+  Brain.playNote(4, 1, 800);
+  Brain.playNote(3, 6, 400);
+  Brain.playNote(3, 4, 400);
+
+  Brain.playNote(4, 0, 800);
+  Brain.playNote(3, 6, 600);
+  Brain.playNote(3, 5, 200);
+  Brain.playNote(3, 6, 800);
+  Brain.playNote(3, 2, 400);
+  Brain.playNote(3, 2, 400);
+
+  Brain.playNote(3, 5, 800);
+  Brain.playNote(3, 4, 600);
+  Brain.playNote(3, 3, 200);
+  Brain.playNote(3, 4, 800);
+  Brain.playNote(3, 0, 400);
+  Brain.playNote(3, 0, 400);
+
+  Brain.playNote(4, 0, 800);
+  Brain.playNote(3, 6, 600);
+  Brain.playNote(3, 5, 200);
+  Brain.playNote(3, 4, 1200);
+  wait(400,msec);
+
+  Brain.playNote(4, 2, 1600);
+  Brain.playNote(4, 1, 400);
+  Brain.playNote(4, 0, 400);
+  Brain.playNote(3, 6, 400);
+  Brain.playNote(4, 0, 400);
+
+  Brain.playNote(4, 1, 1200);
+  Brain.playNote(3, 4, 400);
+  Brain.playNote(3, 4, 1200);
+  wait(400,msec);
+
+  Brain.playNote(4, 0, 1600);
+  Brain.playNote(3, 6, 400);
+  Brain.playNote(3, 5, 400);
+  Brain.playNote(3, 4, 400);
+  Brain.playNote(3, 5, 400);
+
+  Brain.playNote(3, 6, 1200);
+  Brain.playNote(3, 2, 400);
+  Brain.playNote(3, 2, 400);
+  Brain.playNote(3, 4, 400);
+  Brain.playNote(3, 5, 400);
+  Brain.playNote(3, 6, 400);
+
+  Brain.playNote(4, 0, 800);
+  Brain.playNote(3, 5, 600);
+  Brain.playNote(3, 6, 200);
+  Brain.playNote(4, 0, 800);
+  Brain.playNote(3, 5, 600);
+  Brain.playNote(3, 6, 200);
+
+  Brain.playNote(4, 0, 800);
+  Brain.playNote(3, 5, 400);
+  Brain.playNote(4, 0, 400);
+  Brain.playNote(4, 3, 400);
+  Brain.playNote(4, 3, 200);
+  Brain.playNote(4, 3, 200);
+  Brain.playNote(4, 3, 200);
+  Brain.playNote(4, 3, 400);
+  Brain.playNote(4, 3, 400);
+
+  Brain.playNote(4, 3, 1600);
+  
+  
+  
+  
+  
+}
